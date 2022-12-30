c719e724b0ff2bd637a3ccad630293814f1aa413
조소영 <ok69531@gmail.com>
2022-12-30 16:57:40 +0900

    review

--- day6/sy.py
+++ day6/sy.py
@@ -1,1 +1,16 @@
-# https://tutorials.pytorch.kr/intermediate/char_rnn_classification_tutorial
+# 1. https://tutorials.pytorch.kr/intermediate/char_rnn_classification_tutorial
+
+# 2. git brahnch
+# git checkout -b branch_name
+# git branch # branch list 확인
+# git add file_name
+# git commit -m "add comment"
+# git push
+# git checkout main
+# git branch -d branch_name # if the pr is accepted
+# git pull
+
+# 3. server
+# cd 원하는경로
+# sbatch --job-name=name --partition=cpu run_slurm.sh
+# sbatch --job-name=name --partition=gpu1 --gres=gpu:rtx3090:1 run_slurm.sh
