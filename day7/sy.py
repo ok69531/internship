8c16aa1d38a0a936a70c412208a288603a039f32
조소영 <ok69531@gmail.com>
2023-01-01 15:23:23 +0900

    init

---
+++ day7/sy.py
@@ -0,0 +1,7 @@
+# day6 마무리가 안된 경우 마무리후 git PR
+
+# 1. Linear layer 작성
+# class Linear(torch.nn.Modue:)
+
+# 2. 앞서 만든 Linear layer를 이용하여 Logistic regression model 작성
+# Logistic Regression은 이중, 다중 분류가 모두 가능하도록 
---
+++ day8/sy.py
@@ -0,0 +1,3 @@
+# 1. https://towardsdatascience.com/building-rnn-lstm-and-gru-for-time-series-using-pytorch-a46e5b094e7b
+# data: https://www.kaggle.com/datasets/robikscube/hourly-energy-consumption
+# fitting the RNN model constructed in day6
