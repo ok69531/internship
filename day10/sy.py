ad684ab70f13245f0fda73fa7904a30f22b4ae5e
조소영 <ok69531@gmail.com>
2023-01-09 11:26:30 +0900

    add seq2seq link

--- day10/sy.py
+++ day10/sy.py
@@ -1,2 +1,3 @@
+# https://wikidocs.net/24996
 # https://doheon.github.io/%EC%BD%94%EB%93%9C%EA%B5%AC%ED%98%84/time-series/ci-3.lstm-post/
 # https://tutorials.pytorch.kr/intermediate/seq2seq_translation_tutorial.html
