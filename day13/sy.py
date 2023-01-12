9534125ba2e48ad615c960ad3297ab5be3ee314f
조소영 <ok69531@gmail.com>
2023-01-12 13:40:23 +0900

    add link

--- day13/sy.py
+++ day13/sy.py
@@ -1,2 +1,4 @@
 # transformer 실습
-# https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+# transformer 예제 코드
+# https://wandb.ai/wandb_fc/korean/reports/-Transformer---Vmlldzo0MDIyNDc
+# https://pytorch.org/tutorials/beginner/transformer_tutorial.htm
