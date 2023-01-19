8b9159d5f56a2f1734a587cc5bc43515bd12f27d
조소영 <ok69531@gmail.com>
2023-01-19 11:08:16 +0900

    tmp

--- day15/sy.py
+++ day15/sy.py
@@ -1,4 +1,5 @@
 # 수자원 test data에서 급격히 감소하는 부분 예측
+# source code from https://doheon.github.io/코드구현/time-series/ci-4.transformer-post/
 
 import torch
 import torch.nn as nn
@@ -7,86 +8,98 @@ from torch.utils.data import DataLoader, Dataset
 import numpy as np
 import pandas as pd
 import math
-import time
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
 
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
 
 df = pd.read_excel('../수자원.xlsx')
 # df = pd.read_csv('../금융.csv')
 # target = df['ETC-USD'].to_numpy()
+feature = df.drop(['날짜', '저수량'], axis = 1).to_numpy()
 target = df.저수율.to_numpy()
 
 iw = 365
 ow = 120
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 scaler = MinMaxScaler() 
-target = scaler.fit_transform(target.reshape(-1, 1)).reshape(-1)
+feature = scaler.fit_transform(feature)
+target = feature[:, 0]
 
 num_test = -120
-data_train = target[:num_test]
-data_test = target[num_test:]
-# data_train = water[:num_test]
-# data_test = water[num_test:]
+x_train = feature[:num_test]
+x_test = feature[num_test:]
+y_train = target[:num_test]
+y_test = target[num_test:]
 
 
 class windowDataset(Dataset):
-    def __init__(self, y, input_window=80, output_window=20, stride=5):
+    def __init__(self, x, y, input_window=80, output_window=20, stride=5):
         #총 데이터의 개수
         L = y.shape[0]
         #stride씩 움직일 때 생기는 총 sample의 개수
         num_samples = (L - input_window - output_window) // stride + 1
 
         #input과 output
-        X = np.zeros([input_window, num_samples])
+        # X = np.zeros([input_window, num_samples])
+        X = np.zeros([input_window, x.shape[1], num_samples])
         Y = np.zeros([output_window, num_samples])
 
         for i in np.arange(num_samples):
             start_x = stride*i
             end_x = start_x + input_window
-            X[:,i] = y[start_x:end_x]
-
+            X[:, :, i] = feature[start_x:end_x, :]
+            # X[:,i] = y[start_x:end_x]
+            
             start_y = stride*i + input_window
             end_y = start_y + output_window
             Y[:,i] = y[start_y:end_y]
 
-        X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1,0,2))
+        X = X.transpose((2, 0, 1))
+        # X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1,0,2))
         Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1,0,2))
-        self.x = X
-        self.y = Y
+        self.x = torch.tensor(X).to(torch.float32).to(device)
+        self.y = torch.tensor(Y).to(torch.float32).to(device)
         
         self.len = len(X)
+        
     def __getitem__(self, i):
         return self.x[i], self.y[i]
+    
     def __len__(self):
         return self.len
 
 
 class TFModel(nn.Module):
-    def __init__(self,iw, ow, d_model, nhead, nlayers, dropout=0.5):
+    def __init__(self, num_feat, iw, ow, d_model, nhead, nlayers, dropout=0.5):
         super(TFModel, self).__init__()
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=nlayers) 
         self.pos_encoder = PositionalEncoding(d_model, dropout)
 
         self.encoder = nn.Sequential(
-            nn.Linear(1, d_model//2),
-            nn.ReLU(),
+            nn.Linear(num_feat, d_model//2),
+            nn.LeakyReLU(),
             nn.Linear(d_model//2, d_model)
         )
         
         self.linear =  nn.Sequential(
             nn.Linear(d_model, d_model//2),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(d_model//2, 1)
         )
 
         self.linear2 = nn.Sequential(
             nn.Linear(iw, (iw+ow)//2),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear((iw+ow)//2, ow)
         ) 
 
@@ -98,6 +111,7 @@ class TFModel(nn.Module):
     def forward(self, src, srcmask):
         src = self.encoder(src)
         src = self.pos_encoder(src)
+        # output = self.transformer_encoder(src, srcmask)
         output = self.transformer_encoder(src.transpose(0,1), srcmask).transpose(0,1)
         output = self.linear(output)[:,:,0]
         output = self.linear2(output)
@@ -110,8 +124,8 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).to(torch.float32) * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
@@ -120,6 +134,7 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
+
 
 def gen_attention_mask(x):
     mask = torch.eq(x, 0)
@@ -179,21 +194,27 @@ def gen_attention_mask(x):
 
 
 lr = 1e-3
+num_feat = x_train.shape[1]
 d_model = 64
 nhead = 2
-nhid = 64
+# nhid = 64
 nlayers = 2
 dropout = 0.1
 
-model = TFModel(iw, ow, d_model, nhead, nlayers, dropout).to(device)
+model = TFModel(num_feat, iw, ow, d_model, nhead, nlayers, dropout).to(device)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-train_dataset = windowDataset(data_train, input_window=iw, output_window=ow, stride=3)
+train_dataset = windowDataset(x_train, y_train, input_window=iw, output_window=ow, stride=3)
 train_loader = DataLoader(train_dataset, batch_size=64)
 
+# for b in train_loader:
+#     inputs, outputs = b
+#     break
+# inputs.size()
+# outputs.size()
 
-epoch = 100
+epoch = 10
 model.train()
 progress = tqdm(range(epoch))
 for i in progress:
@@ -201,7 +222,7 @@ for i in progress:
     for (inputs, outputs) in train_loader:
         optimizer.zero_grad()
         src_mask = model.generate_square_subsequent_mask(inputs.shape[1]).to(device)
-        result = model(inputs.float().to(device),  src_mask)
+        result = model(inputs, src_mask)
         loss = criterion(result, outputs[:,:,0].float().to(device))
         loss.backward()
         optimizer.step()
@@ -209,18 +230,19 @@ for i in progress:
     progress.set_description("loss: {:0.6f}".format(batchloss.cpu().item() / len(train_loader)))
 
 
-def evaluate():
-    input = torch.tensor(data_train[-iw:]).reshape(1,-1,1).to(device).float().to(device)
-    model.eval()
+# def evaluate():
+#     model.eval()
     
-    src_mask = model.generate_square_subsequent_mask(input.shape[1]).to(device)
-    predictions = model(input, src_mask)
-    return predictions.detach().cpu().numpy()
+#     inputs = torch.tensor(x_test.reshape(1,-1,num_feat)).to(torch.float32).to(device)
+#     src_mask = model.generate_square_subsequent_mask(inputs.shape[1]).to(device)
+#     pred = model(inputs, src_mask)
+    
+#     return pred.detach().cpu()
 
 
 result = evaluate()
 result = scaler.inverse_transform(result)[0]
-real = scaler.inverse_transform(data_test.reshape(-1, 1)).reshape(-1)
+real = scaler.inverse_transform(y_test.reshape(-1, 1)).reshape(-1)
 
 
 plt.figure(figsize=(20,5))
